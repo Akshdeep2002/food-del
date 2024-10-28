8b518326ad0fdbb72181d4d820a2d14c54c46932
Akshdeep2002 <151216116+Akshdeep2002@users.noreply.github.com>
2024-10-28 21:52:59 +0530

    Update App.jsx

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -9,7 +9,7 @@ import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 const App = () => {
-  const url = "https://food-del-frontend-brbk.onrender.com";
+  const url = "https://food-del-backend-ct57.onrender.com";
 
   return (
     <div>
