d8ae701056b715093f1ca5833c9c3d35e148149e
Akshdeep Yadav <akshdeep3602@gmailcom>
2024-10-28 20:26:55 +0530

    first-commit

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import "./Header.css"
+import { assets } from '../../assets/assets'
 
 const Header = () => {
   return (
     <div className='header'>
+      <img src={assets.header_img} alt="" />
       <div className="header-contents">
         <h2>Order your favourite food here</h2>
         <p>Choose from a diverse menu featuring a delectable array of dishescrafted with the finest ingredients and culinary expertise. Our aim is to satisfy your cravings and elevate your dining experience.</p>
